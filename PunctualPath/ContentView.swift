41de9376e55775b1c02dddac5220cbb6b874c2b4
Jiaqi Zhao (Roy) <72789180+JiaqiZhao2004@users.noreply.github.com>
2024-06-25 14:23:19 +0800

    created simple countdown timer

--- PunctualPath/ContentView.swift
+++ PunctualPath/ContentView.swift
@@ -8,14 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var timeRemaining = 100
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            VStack {
+                LineSearchView()
+                Text("Time: \(timeRemaining)")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 5)
+                    .background(.black.opacity(0.75))
+                    .clipShape(.capsule)
+            }
+            
+        }.onReceive(timer) { time in
+            if timeRemaining > 0 {
+                timeRemaining -= 1
+            }
         }
-        .padding()
     }
 }
 
